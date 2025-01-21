9921f8b57fc615a466906115995cbf949aab7980
Trần Văn Sơn <42969529+synasapmob@users.noreply.github.com>
2025-01-21 10:09:44 +0700

    Feat/rj1e i47z (#7)
    * applied earn page eventually responsive
    
    * added earn home instead news

---
+++ client/src/earn.ts
@@ -0,0 +1,5 @@
+import "../node_modules/@fortawesome/fontawesome-free/css/brands.css";
+import "../node_modules/@fortawesome/fontawesome-free/css/fontawesome.css";
+import "../node_modules/@fortawesome/fontawesome-free/css/solid.css";
+import "./scss/pages/client.scss";
+import "./scss/pages/earn.scss";
--- client/vite/vite.common.ts
+++ client/vite/vite.common.ts
@@ -12,6 +12,7 @@ const config: UserConfig = {
                 main: resolve(__dirname, "../index.html"),
                 changelog: resolve(__dirname, "../changelog/index.html"),
                 news: resolve(__dirname, "../news/index.html"),
+                earn: resolve(__dirname, "../earn/index.html"),
                 rules: resolve(__dirname, "../rules/index.html"),
                 editor: resolve(__dirname, "../editor/index.html"),
                 wiki: resolve(__dirname, "../wiki/index.html")
