ced4c7342de1f2a32d9623732cf7f6b72b1adbd1
Stefan <mutobui@gmail.com>
2025-06-17 17:03:33 +0700

    update pm2

--- config/pm2/pm2.config.js
+++ config/pm2/pm2.config.js
@@ -11,7 +11,21 @@ module.exports = {
             env_development: {
                 NODE_ENV: 'development',
                 watch: true,
-                args: 'dev', // Use nodemon for development
+            },
+        },
+        {
+            name: 'surviv-api',
+            script: 'npm',
+            args: 'run start',
+            cwd: './surviv-api',
+            watch: false,
+            env: {
+                NODE_ENV: 'production',
+            },
+            env_development: {
+                NODE_ENV: 'development',
+                watch: true,
+                args: 'run dev',
             },
         },
     ],
