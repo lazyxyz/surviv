46136e7d7ad596d8a89d63d449247c8507c78575
Stefan <mutobui@gmail.com>
2025-06-20 18:40:04 +0700

    update default skins

---
+++ client/public/mapping/SurvivMemes.js
@@ -0,0 +1,17 @@
+const emotesMapping = {
+  "address": "I will insert later",
+  "assets": [
+    "fawkes_mask",
+    "forever_alone",
+    "y_u_no",
+    "fu_meme",
+    "harold",
+    "i_known_bro",
+    "me_gusta",
+    "mr_bean",
+    "okay_guy",
+    "too_small",
+    "trollface"
+  ]
+};
+module.exports = emotesMapping;
