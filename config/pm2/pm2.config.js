e4e24cc71bcfe86bef6b8e081abfd4041cb9b11e
Stefan <mutobui@gmail.com>
2025-07-23 11:35:23 +0700

    pm2 to fork mode

--- config/pm2/pm2.config.js
+++ config/pm2/pm2.config.js
@@ -1,13 +1,14 @@
 module.exports = {
-    apps: [
-        {
-            name: 'surviv',
-            script: 'pnpm',
-            args: 'start',
-            env: { NODE_ENV: 'production' },
-            max_memory_restart: '50G', // Restart if RSS exceeds 50GB
-            instances: 1, // Single process
-            autorestart: true // Restart on crash
-        }
-    ]
+  apps: [
+    {
+      name: 'surviv',
+      script: 'pnpm',
+      args: 'dev',
+      env: { NODE_ENV: 'production' },
+      max_memory_restart: '50G',
+      autorestart: true,
+      exec_mode: 'fork', // Keep cluster mode as per pm2 show
+      time: true, // Add timestamps for clarity in console
+    },
+  ],
 };
