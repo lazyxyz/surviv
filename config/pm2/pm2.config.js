c4b18a40c99fb99cfaa91f5da4bc848c6ae075b7
Stefan <mutobui@gmail.com>
2025-07-23 10:58:25 +0700

    update pm2

--- config/pm2/pm2.config.js
+++ config/pm2/pm2.config.js
@@ -4,10 +4,10 @@ module.exports = {
             name: 'surviv',
             script: 'pnpm',
             args: 'start',
-            watch: false,
-            env: {
-                NODE_ENV: 'production',
-            },
+            env: { NODE_ENV: 'production' },
+            max_memory_restart: '50G', // Restart if RSS exceeds 50GB
+            instances: 1, // Single process
+            autorestart: true // Restart on crash
         }
-    ],
+    ]
 };
