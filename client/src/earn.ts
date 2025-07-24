75164ceb6336906cd395c8b364c91bccad9cf03b
Alex <76422286+alexxpham@users.noreply.github.com>
2025-07-24 09:11:53 +0700

    leaderboard (#54)

--- client/src/earn.ts
+++ client/src/earn.ts
@@ -2,4 +2,5 @@ import "../node_modules/@fortawesome/fontawesome-free/css/brands.css";
 import "../node_modules/@fortawesome/fontawesome-free/css/fontawesome.css";
 import "../node_modules/@fortawesome/fontawesome-free/css/solid.css";
 import "./scss/pages/client.scss";
+import "./scss/pages/leaderboard.scss";
 import "./scss/pages/earn.scss";
