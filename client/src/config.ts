3e841aa8c92e3cfe03a28d13f8f01652274c9f42
Alex Pham <kopmonster16@gmail.com>
2025-08-31 12:00:03 +0700

    update skins, rewards modal

--- client/src/config.ts
+++ client/src/config.ts
@@ -3,14 +3,14 @@ import { toBeHex } from "ethers";
 
 export const Config = {
     regions: {
-        // dev: {
-        //     name: "Local Server",
-        //     mainAddress: "http://127.0.0.1:8000",
-        //     gameAddress: "ws://127.0.0.1:<ID>",
-        //     teamAddress: "ws://127.0.0.1:8000",
-        //     apiAddress: "https://admin.surviv.fun",
-        //     // apiAddress: "http://localhost:3001",
-        // },
+        dev: {
+            name: "Local Server",
+            mainAddress: "http://127.0.0.1:8000",
+            gameAddress: "ws://127.0.0.1:<ID>",
+            teamAddress: "ws://127.0.0.1:8000",
+            apiAddress: "https://admin.surviv.fun",
+            // apiAddress: "http://localhost:3001",
+        },
         // as: {
         //     name: "Asia",
         //     mainAddress: "https://as.surviv.fun",
