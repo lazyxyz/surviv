9626515628a98f0cff70d4142278075b8a618db9
Alex <76422286+alexanddev@users.noreply.github.com>
2025-11-03 17:27:22 +0700

    add airdrop plane cursedIsland sound (#226)

--- client/src/scripts/game.ts
+++ client/src/scripts/game.ts
@@ -280,6 +280,8 @@ export class Game {
             this.fogOfWar.init();
         }
 
+        this.soundManager.loadSounds(gameMode);
+
         await loadTextures(
             pixi.renderer,
             this.inputManager.isMobile
